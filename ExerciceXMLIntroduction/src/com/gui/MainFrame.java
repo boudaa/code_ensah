e2557656aaeb7c365c5d14605d3ebd67e3338fac
BOUDAA <hp@hp-HP.Home>
2017-12-27 19:25:15 +0000

    petite mise à jour

--- ExerciceXMLIntroduction/src/com/gui/MainFrame.java
+++ ExerciceXMLIntroduction/src/com/gui/MainFrame.java
@@ -189,6 +189,7 @@ public class MainFrame extends JFrame {
 	}
 
 	public static void main(String[] args) {
+		displayUtils("début méthode main");
 
 		// On créer la fentre dans l'EDT (Event Dispatch Thread)
 
